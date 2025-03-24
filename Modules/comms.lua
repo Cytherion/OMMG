2e6d182b861499251d53244b3def2ee464cd65d8
Cytherion <66185174+Cytherion@users.noreply.github.com>
2025-03-24 16:23:05 -0400

    update\

--- Modules/comms.lua
+++ Modules/comms.lua
@@ -1,12 +1,13 @@
 -- modules/comms.lua
-
 local mq = require('mq')
 local logger = require('utils.logger')
-local missionManager = require('modules.mission_manager')
+-- Removed: local missionManager = require('modules.mission_manager')
 
 local comms = {}
 
 function comms.broadcast(message)
+    -- Lazy-load missionManager when needed to break the circular dependency.
+    local missionManager = require('modules.mission_manager')
     local plugin = missionManager.getCoordinationPlugin()
     if plugin == "MQ2DanNet" then
         mq.cmdf("/dannet broadcast %s", message)
